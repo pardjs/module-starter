2489e94670e16bbb0e8af0ef87f6d03651fee4e3
Bain <bgd_sh@hotmail.com>
2018-08-05 15:49:10 +0800

    backend part

---
+++ backend/module-starter-backend/src/index.ts
@@ -0,0 +1,5 @@
+// This Module can be used as a single self-start application.
+// Or it can be used as a component of a project.
+export * from './app.controller';
+export * from './app.module';
+export * from './app.service';
